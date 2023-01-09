6cf2460f98ab6abcc6d77babb8f8127b68f6f1ed
srdtrk <srdtrk@hotmail.com>
2023-01-09 19:42:36 +0300

    make move tests updated

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -43,34 +43,6 @@ func TestPlayMove(t *testing.T) {
 	}, *playMoveResponse)
 }
 
-func TestPlayMoveSavedGame(t *testing.T) {
-	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
-	ctx := sdk.UnwrapSDKContext(context)
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator:   bob,
-		GameIndex: "1",
-		FromX:     1,
-		FromY:     2,
-		ToX:       2,
-		ToY:       3,
-	})
-	systemInfo, found := keeper.GetSystemInfo(ctx)
-	require.True(t, found)
-	require.EqualValues(t, types.SystemInfo{
-		NextId: 2,
-	}, systemInfo)
-	game1, found := keeper.GetStoredGame(ctx, "1")
-	require.True(t, found)
-	require.EqualValues(t, types.StoredGame{
-		Index:     "1",
-		Board:     "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "r",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 1,
-	}, game1)
-}
-
 func TestPlayMoveGameNotFound(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
@@ -85,6 +57,88 @@ func TestPlayMoveGameNotFound(t *testing.T) {
 	require.Equal(t, "2: game by id not found", err.Error())
 }
 
+func TestPlayMoveSameBlackRed(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     bob,
+	})
+	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgPlayMoveResponse{
+		CapturedX: -1,
+		CapturedY: -1,
+		Winner:    "*",
+	}, *playMoveResponse)
+}
+
+func TestPlayMoveSavedGame(t *testing.T) {
+	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        2,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "1",
+	}, systemInfo)
+	game1, found := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "r",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   1,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
+	}, game1)
+}
+
+func TestPlayMoveEmitted(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "move-played",
+		Attributes: []sdk.Attribute{
+			{Key: "creator", Value: bob},
+			{Key: "game-index", Value: "1"},
+			{Key: "captured-x", Value: "-1"},
+			{Key: "captured-y", Value: "-1"},
+			{Key: "winner", Value: "*"},
+		},
+	}, event)
+}
+
 func TestPlayMoveNotPlayer(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
@@ -97,6 +151,27 @@ func TestPlayMoveNotPlayer(t *testing.T) {
 	})
 	require.Nil(t, playMoveResponse)
 	require.Equal(t, alice+": message creator is not a player", err.Error())
+}
+
+func TestPlayMoveCannotParseGame(t *testing.T) {
+	msgServer, k, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	storedGame, _ := k.GetStoredGame(ctx, "1")
+	storedGame.Board = "not a board"
+	k.SetStoredGame(ctx, storedGame)
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, r, "game cannot be parsed: invalid board string: not a board")
+	}()
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
 }
 
 func TestPlayMoveWrongOutOfTurn(t *testing.T) {
@@ -175,18 +250,55 @@ func TestPlayMove2SavedGame(t *testing.T) {
 	systemInfo, found := keeper.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{
-		NextId: 2,
+		NextId:        2,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "1",
 	}, systemInfo)
 	game1, found := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Index:     "1",
-		Board:     "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 2,
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   2,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
 	}, game1)
+}
+
+func TestPlayMove2Emitted(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   carol,
+		GameIndex: "1",
+		FromX:     0,
+		FromY:     5,
+		ToX:       1,
+		ToY:       4,
+	})
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.Equal(t, "move-played", event.Type)
+	require.EqualValues(t, []sdk.Attribute{
+		{Key: "creator", Value: carol},
+		{Key: "game-index", Value: "1"},
+		{Key: "captured-x", Value: "-1"},
+		{Key: "captured-y", Value: "-1"},
+		{Key: "winner", Value: "*"},
+	}, event.Attributes[5:])
 }
 
 func TestPlayMove3(t *testing.T) {
@@ -253,97 +365,20 @@ func TestPlayMove3SavedGame(t *testing.T) {
 	systemInfo, found := keeper.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{
-		NextId: 2,
+		NextId:        2,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "1",
 	}, systemInfo)
 	game1, found := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Index:     "1",
-		Board:     "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "r",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 3,
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "r",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   3,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
 	}, game1)
-}
-
-func TestPlayMoveCannotParseGame(t *testing.T) {
-	msgServer, k, context := setupMsgServerWithOneGameForPlayMove(t)
-	ctx := sdk.UnwrapSDKContext(context)
-	storedGame, _ := k.GetStoredGame(ctx, "1")
-	storedGame.Board = "not a board"
-	k.SetStoredGame(ctx, storedGame)
-	defer func() {
-		r := recover()
-		require.NotNil(t, r, "The code did not panic")
-		require.Equal(t, r, "game cannot be parsed: invalid board string: not a board")
-	}()
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator:   bob,
-		GameIndex: "1",
-		FromX:     1,
-		FromY:     2,
-		ToX:       2,
-		ToY:       3,
-	})
-}
-
-func TestPlayMoveEmitted(t *testing.T) {
-	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator:   bob,
-		GameIndex: "1",
-		FromX:     1,
-		FromY:     2,
-		ToX:       2,
-		ToY:       3,
-	})
-	ctx := sdk.UnwrapSDKContext(context)
-	require.NotNil(t, ctx)
-	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
-	require.Len(t, events, 2)
-	event := events[0]
-	require.EqualValues(t, sdk.StringEvent{
-		Type: "move-played",
-		Attributes: []sdk.Attribute{
-			{Key: "creator", Value: bob},
-			{Key: "game-index", Value: "1"},
-			{Key: "captured-x", Value: "-1"},
-			{Key: "captured-y", Value: "-1"},
-			{Key: "winner", Value: "*"},
-		},
-	}, event)
-}
-
-func TestPlayMove2Emitted(t *testing.T) {
-	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator:   bob,
-		GameIndex: "1",
-		FromX:     1,
-		FromY:     2,
-		ToX:       2,
-		ToY:       3,
-	})
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator:   carol,
-		GameIndex: "1",
-		FromX:     0,
-		FromY:     5,
-		ToX:       1,
-		ToY:       4,
-	})
-	ctx := sdk.UnwrapSDKContext(context)
-	require.NotNil(t, ctx)
-	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
-	require.Len(t, events, 2)
-	event := events[0]
-	require.Equal(t, "move-played", event.Type)
-	require.EqualValues(t, []sdk.Attribute{
-		{Key: "creator", Value: carol},
-		{Key: "game-index", Value: "1"},
-		{Key: "captured-x", Value: "-1"},
-		{Key: "captured-y", Value: "-1"},
-		{Key: "winner", Value: "*"},
-	}, event.Attributes[5:])
 }
