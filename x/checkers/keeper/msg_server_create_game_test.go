9f143e78546386be5242fade7b95a4b578baec7c
srdtrk <srdtrk@hotmail.com>
2023-01-09 19:40:27 +0300

    create game tests updated

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -12,6 +12,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
 func TestCreateGame(t *testing.T) {
 	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
@@ -27,103 +33,75 @@ func TestCreateGame(t *testing.T) {
 
 func TestCreate1GameHasSaved(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
-	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	systemInfo, found := keeper.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{
-		NextId: 2,
+		NextId:        2,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "1",
 	}, systemInfo)
-	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index:     "1",
-		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 0,
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
 	}, game1)
 }
 
-func TestCreate3GamesGetAll(t *testing.T) {
+func TestCreate1GameGetAll(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
-	})
-	msgSrvr.CreateGame(context, &types.MsgCreateGame{
-		Creator: bob,
-		Black:   carol,
-		Red:     alice,
-	})
-	msgSrvr.CreateGame(context, &types.MsgCreateGame{
-		Creator: carol,
-		Black:   alice,
-		Red:     bob,
 	})
 	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
-	require.Len(t, games, 3)
+	require.Len(t, games, 1)
 	require.EqualValues(t, types.StoredGame{
-		Index:     "1",
-		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 0,
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
 	}, games[0])
-	require.EqualValues(t, types.StoredGame{
-		Index:     "2",
-		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     carol,
-		Red:       alice,
-		MoveCount: 0,
-	}, games[1])
-	require.EqualValues(t, types.StoredGame{
-		Index:     "3",
-		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     alice,
-		Red:       bob,
-		MoveCount: 0,
-	}, games[2])
 }
 
-func TestCreateGameFarFuture(t *testing.T) {
-	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+func TestCreate1GameEmitted(t *testing.T) {
+	msgSrvr, _, context := setupMsgServerCreateGame(t)
 	ctx := sdk.UnwrapSDKContext(context)
-	systemInfo, found := keeper.GetSystemInfo(ctx)
-	systemInfo.NextId = 1024
-	keeper.SetSystemInfo(ctx, systemInfo)
-	createResponse, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
-	require.Nil(t, err)
-	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "1024",
-	}, *createResponse)
-	systemInfo, found = keeper.GetSystemInfo(ctx)
-	require.True(t, found)
-	require.EqualValues(t, types.SystemInfo{
-		NextId: 1025,
-	}, systemInfo)
-	game1, found1 := keeper.GetStoredGame(ctx, "1024")
-	require.True(t, found1)
-	require.EqualValues(t, types.StoredGame{
-		Index:     "1024",
-		Board:     "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Black:     bob,
-		Red:       carol,
-		MoveCount: 0,
-	}, game1)
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 1)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "new-game-created",
+		Attributes: []sdk.Attribute{
+			{Key: "creator", Value: alice},
+			{Key: "game-index", Value: "1"},
+			{Key: "black", Value: bob},
+			{Key: "red", Value: carol},
+		},
+	}, event)
 }
 
 func TestCreateGameRedAddressBad(t *testing.T) {
@@ -152,31 +130,179 @@ func TestCreateGameEmptyRedAddress(t *testing.T) {
 		err.Error())
 }
 
-func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
-	k, ctx := keepertest.CheckersKeeper(t)
-	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
-	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
-}
-
-func TestCreate1GameEmitted(t *testing.T) {
+func TestCreate3Games(t *testing.T) {
 	msgSrvr, _, context := setupMsgServerCreateGame(t)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
+	createResponse2, err2 := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	require.Nil(t, err2)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "2",
+	}, *createResponse2)
+	createResponse3, err3 := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Black:   alice,
+		Red:     bob,
+	})
+	require.Nil(t, err3)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "3",
+	}, *createResponse3)
+}
+
+func TestCreate3GamesHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
 	ctx := sdk.UnwrapSDKContext(context)
-	require.NotNil(t, ctx)
-	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
-	require.Len(t, events, 1)
-	event := events[0]
-	require.EqualValues(t, sdk.StringEvent{
-		Type: "new-game-created",
-		Attributes: []sdk.Attribute{
-			{Key: "creator", Value: alice},
-			{Key: "game-index", Value: "1"},
-			{Key: "black", Value: bob},
-			{Key: "red", Value: carol},
-		},
-	}, event)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Black:   alice,
+		Red:     bob,
+	})
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        4,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "3",
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: "-1",
+		AfterIndex:  "2",
+	}, game1)
+	game2, found2 := keeper.GetStoredGame(ctx, "2")
+	require.True(t, found2)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "2",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       carol,
+		Red:         alice,
+		MoveCount:   0,
+		BeforeIndex: "1",
+		AfterIndex:  "3",
+	}, game2)
+	game3, found3 := keeper.GetStoredGame(ctx, "3")
+	require.True(t, found3)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "3",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       alice,
+		Red:         bob,
+		MoveCount:   0,
+		BeforeIndex: "2",
+		AfterIndex:  "-1",
+	}, game3)
+}
+
+func TestCreate3GamesGetAll(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Black:   alice,
+		Red:     bob,
+	})
+	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	require.Len(t, games, 3)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: "-1",
+		AfterIndex:  "2",
+	}, games[0])
+	require.EqualValues(t, types.StoredGame{
+		Index:       "2",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       carol,
+		Red:         alice,
+		MoveCount:   0,
+		BeforeIndex: "1",
+		AfterIndex:  "3",
+	}, games[1])
+	require.EqualValues(t, types.StoredGame{
+		Index:       "3",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       alice,
+		Red:         bob,
+		MoveCount:   0,
+		BeforeIndex: "2",
+		AfterIndex:  "-1",
+	}, games[2])
+}
+
+func TestCreateGameFarFuture(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	systemInfo.NextId = 1024
+	keeper.SetSystemInfo(ctx, systemInfo)
+	createResponse, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "1024",
+	}, *createResponse)
+	systemInfo, found = keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        1025,
+		FifoHeadIndex: "1024",
+		FifoTailIndex: "1024",
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(ctx, "1024")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "1024",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: "-1",
+		AfterIndex:  "-1",
+	}, game1)
 }
