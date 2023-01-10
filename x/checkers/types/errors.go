cd26671635c3c8dc3cd0d95fb3357271ee8a2ff8
srdtrk <srdtrk@hotmail.com>
2023-01-10 16:08:04 +0300

    added new error for when we cannot find the winner during forfeit

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -23,4 +23,6 @@ var (
 	ErrInvalidDeadline = sdkerrors.Register(ModuleName, 1109, "deadline cannot be parsed: %s")
 
 	ErrGameFinished = sdkerrors.Register(ModuleName, 1110, "game is already finished")
+
+	ErrCannotFindWinnerByColor = sdkerrors.Register(ModuleName, 1111, "cannot find winner by color: %s")
 )
