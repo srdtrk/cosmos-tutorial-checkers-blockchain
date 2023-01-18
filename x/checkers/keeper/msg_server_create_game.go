ac5bc10c2f96cb8086a11d02aeb4a39285ca6e82
srdtrk <srdtrk@hotmail.com>
2023-01-18 10:25:57 +0300

    implemented handling of the new denom - surprisingly simple

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -32,6 +32,7 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 		Deadline:    types.FormatDeadline(types.GetNextDeadline(ctx)),
 		Winner:      rules.PieceStrings[rules.NO_PLAYER],
 		Wager:       msg.Wager,
+		Denom:       msg.Denom,
 	}
 
 	// make sure the addresses black and red are valid
@@ -57,6 +58,7 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 			sdk.NewAttribute(types.GameCreatedEventBlack, msg.Black),
 			sdk.NewAttribute(types.GameCreatedEventRed, msg.Red),
 			sdk.NewAttribute(types.GameCreatedEventWager, strconv.FormatUint(msg.Wager, 10)),
+			sdk.NewAttribute(types.GameCreatedEventDenom, msg.Denom),
 		),
 	)
 
